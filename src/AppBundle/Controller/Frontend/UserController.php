f1fe254cfe85c92af3be7d33271ee8d6523a1020
DesirHeavy <desir.pembele@heavy-drive.com>
2017-05-19 00:26:32 +0200

    [hotfix] login and registration template (#13)

---
+++ src/AppBundle/Controller/Frontend/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller\Frontend;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller{
+    /**
+     * @Route("/registration", name="Registration")
+     *
+     */
+    public function registrationAction(){
+
+        return $this->render('frontend/register.html.twig', []);
+    }
+
+    /**
+     * @Route("/connection", name="connection")
+     *
+     */
+    public function connectionAction(){
+
+        return $this->render('frontend/connection.html.twig', []);
+    }
+}
