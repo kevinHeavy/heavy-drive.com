dca8939ea9c796d0e72c8841875439529814a4e0
LubinAshley <ashleyl@heavy-drive.com>
2017-08-01 18:40:49 +0200

    [FosUserBundle] adapt bundle for this project

--- src/AppBundle/Controller/Frontend/UserController.php
+++ src/AppBundle/Controller/Frontend/UserController.php
@@ -9,21 +9,5 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller{
-    /**
-     * @Route("/registration", name="Registration")
-     *
-     */
-    public function registrationAction(){
 
-        return $this->render('frontend/user/register.html.twig', []);
-    }
-
-    /**
-     * @Route("/connection", name="connection")
-     *
-     */
-    public function connectionAction(){
-
-        return $this->render('frontend/user/connection.html.twig', []);
-    }
 }
