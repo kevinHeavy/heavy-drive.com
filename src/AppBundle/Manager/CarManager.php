e20c69ae429cffcf4a6454bd056045de14738f3f
LubinAshley <ashleyl@heavy-drive.com>
2017-08-08 00:54:41 +0200

    [Config.Manager] Change config and create carManager

--- src/AppBundle/Manager/CarManager.php
+++ src/AppBundle/Manager/CarManager.php
@@ -9,7 +9,81 @@
 namespace AppBundle\Manager;
 
 
+use AppBundle\Entity\Car;
+use AppBundle\Entity\Client;
+use AppBundle\Entity\Price;
+use AppBundle\Entity\Reservation;
+use AppBundle\Entity\Service;
+use AppBundle\Repository\CarRepository;
+use AppBundle\Repository\PriceRepository;
+use AppBundle\Repository\ReservationRepository;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+
 class CarManager
 {
+    /**
+     * @var Registry
+     */
+    protected $registry;
 
+    /**
+     * @var ReservationRepository
+     */
+    protected $reservationRepository;
+
+    /**
+     * @var PriceRepository
+     */
+    protected $priceRepository;
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * CarManager constructor.
+     *
+     * @param Registry      $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry              = $registry;
+        $this->entityManager         = $registry->getManagerForClass(Reservation::class);
+        $this->reservationRepository = $registry->getRepository(Reservation::class);
+        $this->priceRepository       = $registry->getRepository(Price::class);
+        //$this->priceRepository         = $registry->getRepository(Price::class);
+    }
+
+    /**
+     * List my booking cars was done.
+     *
+     * @param Client $user
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function listHistoricBookingCars(Client $user)
+    {
+        return $this->reservationRepository->getReservationHistoricByClient($user)->getQuery()->getResult();
+    }
+
+    /**
+     * List my booking cars.
+     *
+     * @param Client $user
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function listBookingCars(Client $user)
+    {
+        return $this->reservationRepository->getReservationByClient($user)->getQuery()->getResult();
+    }
+
+    public function listPriceCarByService($service, $car)
+    {
+        return $this->priceRepository->getPriceCarByService($service, $car)->getQuery()->getResult();
+    }
 }
+
