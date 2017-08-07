98d8ef9f6b7540b18c3de6b049b5cb6f4e792201
LubinAshley <ashleyl@heavy-drive.com>
2017-08-08 01:02:03 +0200

    [controlle&translation] create route for acces to my account

--- src/AppBundle/Controller/Frontend/CarController.php
+++ src/AppBundle/Controller/Frontend/CarController.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Controller\Frontend;
 
+use AppBundle\Entity\Car;
+use AppBundle\Entity\Price;
+use AppBundle\Entity\Reservation;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class CarController extends Controller
 {
@@ -23,5 +27,51 @@ class CarController extends Controller
     public function detailsAction()
     {
         return $this->render('frontend/car/details.html.twig');
+    }
+
+    /**
+     * list of cars was booking
+     *
+     * @Route("/my-booking-done", name="cars_booking_done")
+     * @Security("is_granted('IS_AUTHENTICATED_ANONYMOUSLY')")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function listOfReservationDone(Request $request)
+    {
+        $carManager = $this->container->get('heavy.manager.car');
+
+        $user = $this->getUser();
+
+        $carsHasBooking = $carManager->listHistoricBookingCars($user);
+
+        return $this->render("frontend\car\listHistoricBookingCars.html.twig", [
+            "carHasBooking" => $carsHasBooking
+        ]);
+    }
+
+    /**
+     * list of cars booking
+     *
+     * @Route("/my-booking", name="cars_booking")
+     * @Security("is_granted('IS_AUTHENTICATED_ANONYMOUSLY')")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function listOfReservation(Request $request)
+    {
+        $carManager = $this->container->get('heavy.manager.car');
+
+        $user = $this->getUser();
+
+        $carsBooking = $carManager->listBookingCars($user);
+
+        return $this->render("frontend\car\listBookingCar.html.twig", [
+            "carsBooking" => $carsBooking
+        ]);
     }
 }
